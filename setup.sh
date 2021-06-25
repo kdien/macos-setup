d135aa5ee26494780fb45f93645843ee8acf58c7
Khoa Dien <khoa.dien@hotmail.com>
2021-06-24 23:50:02 -0300

    Add powerlevel10k to setup script

--- setup.sh
+++ setup.sh
@@ -6,12 +6,15 @@ git clone https://github.com/kdien/dotfiles.git $HOME/dotfiles
 ln -sf $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -sf $HOME/dotfiles/zsh/.zsh_aliases $HOME/.zsh_aliases
 ln -sf $HOME/dotfiles/zsh/.zsh_functions $HOME/.zsh_functions
+ln -sf $HOME/dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
 ln -sf $HOME/dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
+ln -sf /opt/homebrew/opt/powerlevel10k $HOME/powerlevel10k
 
 # Tap additional brew repos
 brew tap homebrew/cask-fonts
 brew tap homebrew/cask-drivers
+brew tap romkatv/powerlevel10k
 
 # Install brew formulae
 brew install $(cat ./formulae)
