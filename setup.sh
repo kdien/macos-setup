69d296f4ca10eb53ba801bd825343e029025d9a6
Khoa Dien <khoa.dien@bounteous.com>
2022-09-19 15:56:42 -0300

    Multiple updates for dotfiles and packages to install

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Hide dock and remove delay
 defaults write com.apple.dock autohide -bool true
@@ -12,28 +12,34 @@ defaults -currentHost write -g AppleFontSmoothing -int 1
 # Enable Debug menu in Safari
 defaults write com.apple.Safari IncludeInternalDebugMenu 1
 
+# Get pureline bash prompt
+git clone https://github.com/chris-marsh/pureline.git "$HOME"/pureline
+
 # Clone dotfiles and setup symlinks
-git clone https://github.com/kdien/dotfiles.git $HOME/dotfiles
-[[ -f $HOME/.zshrc ]] && mv $HOME/.zshrc $HOME/.zshrc.bak
-ln -sf $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
-ln -sf $HOME/dotfiles/zsh/.zsh_aliases $HOME/.zsh_aliases
-ln -sf $HOME/dotfiles/zsh/.zsh_functions $HOME/.zsh_functions
-ln -sf $HOME/dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
-ln -sf $HOME/dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
-ln -sf $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
-ln -sf $(brew --prefix)/opt/powerlevel10k $HOME/powerlevel10k
+git clone https://github.com/kdien/dotfiles.git "$HOME"/dotfiles
+ln -sf "$HOME"/dotfiles/tmux/.tmux.conf "$HOME"/.tmux.conf
+ln -sf "$HOME"/dotfiles/vim/.vimrc "$HOME"/.vimrc
+mkdir -p "$HOME"/.vim
+ln -sf "$HOME"/dotfiles/vim/coc-settings.json "$HOME"/.vim/coc-settings.json
+ln -sf "$HOME"/dotfiles/alacritty "$HOME"/.config/alacritty
+ln -sf "$HOME"/dotfiles/pureline/.pureline.conf "$HOME"/.pureline.conf
+ln -sf "$HOME"/dotfiles/powershell "$HOME"/.config/powershell
+
+cat >> "$HOME"/.bashrc <<'EOF'
+export PATH="$(brew --prefix)/opt/coreutils/libexec/gnubin:$PATH"
+[[ -f "$HOME"/dotfiles/bash/.bash_common ]] && . "$HOME"/dotfiles/bash/.bash_common
+EOF
 
 # Get baseline git config
-cp $HOME/dotfiles/git/config $HOME/.gitconfig
+cp "$HOME"/dotfiles/git/config "$HOME"/.gitconfig
 
 # Get ssh config
-mkdir -p $HOME/.ssh
-cat $HOME/dotfiles/ssh/config >> $HOME/.ssh/config
+mkdir -p "$HOME"/.ssh
+cat "$HOME"/dotfiles/ssh/config >> "$HOME"/.ssh/config
 
 # Tap additional brew repos
 brew tap homebrew/cask-fonts
 brew tap homebrew/cask-drivers
-brew tap romkatv/powerlevel10k
 
 # Install brew formulae
 brew install $(cat ./formulae)
@@ -43,4 +49,3 @@ brew install --cask $(cat ./casks)
 
 # Set up symlink for OpenJDK11
 sudo ln -sfn $(brew --prefix)/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
-
